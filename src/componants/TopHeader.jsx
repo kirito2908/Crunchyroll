da0dfbffdec09eef87678a8d19bbaeae73b51ff9
user <user@users-MacBook-Pro.local>
2024-05-14 22:19:00 +0530

    update

---
+++ src/componants/TopHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TopHeader() {
+  return (
+    <>
+      <div className="top_header">
+        <div className="upper_left_part">
+          <i className="ri-menu-line"></i>
+          <p>Menu</p>
+        </div>
+        <div className="middle_part">
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TopHeader;
