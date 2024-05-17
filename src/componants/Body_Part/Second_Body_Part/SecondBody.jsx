acafd0d45c0c4ca0acad27e2e88521cc96d91e17
kirito2908 <kazutokirigaya2908@gmail.com>
2024-05-17 11:51:16 +0530

    nextProgress

--- src/componants/Body_Part/Second_Body_Part/SecondBody.jsx
+++ src/componants/Body_Part/Second_Body_Part/SecondBody.jsx
@@ -5,15 +5,20 @@ const SecondBody = () => {
   return (
     <div className="container" style={Second_Body}>
       <div className="second_main">
+        <div className="coverup">
+          <div className="gradient_top"></div>
+          <img src="https://www.crunchyroll.com/animeawards/static/vector-top-lg-89b9d27de020bf6dd07d9d8f1fc4c17d.png" alt="CoverUp"/>
+          <img id="dustIMG" src="/assets/dust.png" alt="The_Dust"/>
+          <img id="largeStar" src="/assets/large_star.png" alt="starLG"/>
+          <img id="smallStar" src="/assets/small_star.png" alt="starSM"/>
+          <video src="/assets/top-thread.mp4" autoPlay loop playsInline muted></video>
+        </div>
         <div className="text">
           <h1 className="theText1">The Crunchyroll Anime Awards</h1>
           <i className="ri-shining-fill"></i>
           <h1 className="theText2">And Your Winners Are…</h1>
         </div>
       </div>
-      {/* <div className="starlg">
-            <img src="https://www.crunchyroll.com/animeawards/static/star-2x2-lg-82c1b94e4c09728396efa02975df0ff9.png" alt="largeStar"/>
-        </div> */}
     </div>
   );
 };
