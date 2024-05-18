ac4091769e28afad7e7e11947a9948caffae258c
kirito2908 <kazutokirigaya2908@gmail.com>
2024-05-18 20:42:05 +0530

    cardDone

--- src/componants/Cards/Single_Card/TheCard.jsx
+++ src/componants/Cards/Single_Card/TheCard.jsx
@@ -1,22 +1,19 @@
 import Card_Part from "./Card_Part.css";
 import React from "react";
 
-export const TheCard = () => {
+export const TheCard = ({ titleName, image, name }) => {
   return (
     <div style={Card_Part}>
       <div className="cardContainer">
         <div className="singleCard">
           <div className="nom">
-            <p>Anime of the Year</p>
+            <p>{titleName}</p>
           </div>
           <div className="poster">
-            <img
-              src="https://a.storyblok.com/f/174976/2000x3000/e071641086/04_jjk_kv_2x3.png/m/500x0/filters:quality(95):format(webp)"
-              alt="Jujutsu Kaisen"
-            />
+            <img src={image} alt={name} />
           </div>
           <div className="title">
-            <p>Jujutsu Kaisen Season 2</p>
+            <p>{name}</p>
             <p className="viewButton">
               View <i class="ri-arrow-right-s-line"></i>
             </p>
